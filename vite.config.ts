780741cc21c02a10af55f6e3e0215a5c7b762611
r3mot <cmendrella1@toromail.csudh.edu>
2025-01-21 09:55:19 -0800

    feat: first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest/config" />
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
+  },
+});
